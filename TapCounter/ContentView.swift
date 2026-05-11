50288795282dda22fb18e4ef6f2adc50569aa837
Дмитрий М. <s1ash@mail.ru>
2026-05-11 18:32:24 +0300

    Add second counter or improve UI

--- TapCounter/ContentView.swift
+++ TapCounter/ContentView.swift
@@ -1,19 +1,39 @@
-//
-//  ContentView.swift
-//  TapCounter
-//
-//  Created by Дмитрий on 11.05.2026.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            Text("Счётчик нажатий")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text("\(count)")
+                .font(.system(size: 70, weight: .bold))
+                .foregroundColor(.blue)
+            
+            Button(action: {
+                count += 1
+            }) {
+                Text("Нажми меня")
+                    .font(.title2)
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+            Button(action: {
+                count = 0
+            }) {
+                Text("Сбросить")
+                    .font(.title2)
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
         .padding()
     }
