3f954f8ef4ec63584d025640ce0eafa66c0167d6
Дмитрий М <s1ash@mail.ru>
2026-05-11 18:06:35 +0300

    Initial Commit

---
+++ TapCounter/TapCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapCounterApp.swift
+//  TapCounter
+//
+//  Created by Дмитрий on 11.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct TapCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
